ed4f60cb06a1ff43b6d0c67744c1354a9855d616
ralexrivero <ralexrivero@gmail.com>
2023-02-18 23:54:57 -0300

    ngFor directive

--- apm/src/app/products/product-list.component.ts
+++ apm/src/app/products/product-list.component.ts
@@ -6,7 +6,7 @@ import { Component } from "@angular/core";
 })
 
 export class ProductListComponent {
-  pageTitle: string = 'Product List!';
+  pageTitle: string = 'Product List';
   products: any[] = [
     {
       "productId": 2,
