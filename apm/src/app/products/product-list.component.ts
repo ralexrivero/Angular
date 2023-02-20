728325c4108c18a29bd3dae5dc4417a2c4f755f1
ralexrivero <ralexrivero@gmail.com>
2023-02-19 21:51:35 -0300

    bind images

--- apm/src/app/products/product-list.component.ts
+++ apm/src/app/products/product-list.component.ts
@@ -7,6 +7,8 @@ import { Component } from "@angular/core";
 
 export class ProductListComponent {
   pageTitle: string = 'Product List';
+  imageWidth: number = 48;
+  imageMargin: number= 2;
   products: any[] = [
     {
       "productId": 2,
